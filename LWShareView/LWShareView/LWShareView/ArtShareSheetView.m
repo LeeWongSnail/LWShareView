e214713be664d80bbf9097f5f3ec9bef19c33335
cainvLuo <624757635@qq.com>
2018-01-09 14:54:11 +0800

    add contentview next step to  complete contentView

--- LWShareView/LWShareView/LWShareView/ArtShareSheetView.m
+++ LWShareView/LWShareView/LWShareView/ArtShareSheetView.m
@@ -12,7 +12,8 @@
 
 @interface ArtShareSheetView ()
 @property (nonatomic, strong) UIButton *cancelBtn;
-
+@property (nonatomic, strong) UIView *contentView;
+@property (nonatomic, strong) UILabel *shareTipLabel;
 @end
 
 @implementation ArtShareSheetView
@@ -20,7 +21,7 @@
 - (instancetype)initWithFrame:(CGRect)frame
 {
     if (self = [super initWithFrame:frame]) {
-        self.backgroundColor = [UIColor redColor];
+        self.backgroundColor = [UIColor clearColor];
         [self buildUI];
     }
     return self;
@@ -34,9 +35,43 @@
         make.bottom.equalTo(self.mas_bottom).offset(-10);
         make.height.equalTo(@50);
     }];
+    
+    [self.contentView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.left.right.equalTo(self);
+        make.bottom.equalTo(self.cancelBtn.mas_top).offset(-10);
+    }];
+    
+    [self.shareTipLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.contentView.mas_top).offset(20);
+        make.centerX.equalTo(self.contentView.mas_centerX);
+    }];
 }
 
 #pragma mark - Lazy Load
+
+- (UILabel *)shareTipLabel
+{
+    if (_shareTipLabel == nil) {
+        _shareTipLabel = [[UILabel alloc] init];
+        _shareTipLabel.font = [UIFont systemFontOfSize:17];
+        _shareTipLabel.textColor = [UIColor colorWithHexString:@"333333"];
+        _shareTipLabel.text = @"分享至";
+        [self.contentView addSubview:_shareTipLabel];
+    }
+    return _shareTipLabel;
+}
+
+- (UIView *)contentView
+{
+    if (_contentView == nil) {
+        _contentView = [[UIView alloc] init];
+        _contentView.backgroundColor = [UIColor colorWithHexString:@"f0f0f0"];
+        _contentView.layer.cornerRadius = 12;
+        _contentView.layer.masksToBounds = YES;
+        [self addSubview:_contentView];
+    }
+    return _contentView;
+}
 
 - (UIButton *)cancelBtn
 {
