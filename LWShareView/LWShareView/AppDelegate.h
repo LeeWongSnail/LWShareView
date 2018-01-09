c35856a9a41f99be76c325d90223b779eb7e9867
cainvLuo <624757635@qq.com>
2018-01-09 11:44:33 +0800

    add init

---
+++ LWShareView/LWShareView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LWShareView
+//
+//  Created by LeeWong on 2018/1/9.
+//  Copyright © 2018年 LeeWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LWShareView/LWShareView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LWShareView
+//
+//  Created by LeeWong on 2018/1/9.
+//  Copyright © 2018年 LeeWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
